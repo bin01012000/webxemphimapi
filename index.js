dcfa5379561c7860cd98c11305d672a7a3454638
Dier <bin01012000@gmail.com>
2022-03-16 20:47:09 +0700

    fix cors

--- index.js
+++ index.js
@@ -46,7 +46,11 @@ app.use("/", routerUser);
 app.use("/", routerFilm);
 app.use("/", routerCategory);
 app.use("/", routerFav);
-
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get("/", (req, res) => {
     res.send('Hello get');
@@ -56,6 +60,8 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
     res.send("Hello post");
 });
+
+
 
 app.post("/upload", cors(), (req, res) => {
     var con = mysql.createConnection({
