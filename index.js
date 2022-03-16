06d9aa28a475bcd4338e8032c84654d6f477b4b8
Dier <bin01012000@gmail.com>
2022-03-16 20:25:51 +0700

    fix cors

--- index.js
+++ index.js
@@ -47,6 +47,12 @@ app.use("/", routerFilm);
 app.use("/", routerCategory);
 app.use("/", routerFav);
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get("/", (req, res) => {
     res.send('Hello get');
 });
@@ -63,8 +69,6 @@ app.post("/upload", (req, res) => {
         password: process.env.PASSWORD,
         database: process.env.DB
     });
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     upload(req, res, (err) => {
         const q = url.parse(req.url, true).query;
 
